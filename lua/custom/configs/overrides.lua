85931075e08cb78449459211a685e49c2bfad163
Roee Jukin <roeej@pm.me>
2023-09-05 08:55:02 +0300

    [gitignore] Remove "custom" directory and "spell" from gitignore

---
+++ lua/custom/configs/overrides.lua
@@ -0,0 +1,88 @@
+local M = {}
+
+M.treesitter = {
+  ensure_installed = {
+    "vim",
+    "lua",
+    "html",
+    "css",
+    "javascript",
+    "typescript",
+    "tsx",
+    "c",
+    "markdown",
+    "markdown_inline",
+    "go",
+    "gomod",
+    "gosum",
+    "typescript",
+    "javascript",
+    "rust",
+    "svelte",
+    "haskell",
+  },
+  indent = {
+    enable = true,
+    -- disable = {
+    --   "python"
+    -- },
+  },
+  incremental_selection = {
+    enable = true,
+    keymaps = {
+      init_selection = "<C-space>",
+      node_incremental = "<c-space>",
+      scope_incremental = "<c-s>",
+      node_decremental = "<M-space>",
+    },
+  },
+}
+
+M.mason = {
+  ensure_installed = {
+    -- lua stuff
+    "lua-language-server",
+    "stylua",
+
+    -- web dev stuff
+    "css-lsp",
+    "html-lsp",
+    "typescript-language-server",
+    "deno",
+    "prettier",
+    "rustywind",
+
+    -- c/cpp stuff
+    "clangd",
+    "clang-format",
+
+    -- golang
+    "gofumpt",
+    "goimports",
+    "gopls",
+
+    -- Svelte
+    "svelte-language-server",
+
+    -- rust
+    "rust-analyzer",
+  },
+}
+
+-- git support in nvimtree
+M.nvimtree = {
+  git = {
+    enable = true,
+  },
+
+  renderer = {
+    highlight_git = true,
+    icons = {
+      show = {
+        git = true,
+      },
+    },
+  },
+}
+
+return M
---
+++ lua/custom/mappings.lua
@@ -0,0 +1,38 @@
+---@type MappingsTable
+local M = {}
+local close_buf = function()
+  require("nvchad.tabufline").close_buffer()
+end
+
+local format_file = function()
+  vim.lsp.buf.format { async = true }
+end
+
+M.general = {
+  n = {
+    [";"] = { ":", "enter command mode", opts = { nowait = true } },
+    ["<leader>ww"] = { "<Cmd>:w<CR>", "[W]rite", opts = { noremap = true } },
+    ["<leader>we"] = { "<Cmd>:wa<CR>", "[W]rite All", opts = { noremap = true } },
+    ["<leader>qq"] = { "<Cmd>:q!<CR>", "[Q]uit", opts = { noremap = true } },
+    ["<leader>bb"] = { "<Cmd>bprevious<CR>", "[B]uffer: Previous" },
+    ["<leader>bn"] = { "<Cmd>bNext<CR>", "[B]uffer: [N]ext" },
+    ["<leader>bq"] = { close_buf, "[B]uffer: [Q]uit" },
+    ["<leader>ff"] = { format_file, "[F]ormat [F]ile" },
+    ["<leader>sf"] = { "<Cmd>Telescope find_files<CR>", "[S]earch [F]iles" },
+    ["<leader>sg"] = { "<Cmd>Telescope live_grep<CR>", "[S]earch [G]rep" },
+    ["gr"] = { "<Cmd>Telescope lsp_references<CR>", "[S]earch [G]rep" },
+    ["<leader><leader>"] = { "<Cmd>Telescope buffers<CR>", "[S]earch Buffers" },
+    ["<leader>sd"] = { "<Cmd>Telescope diagnostics<CR>", "[S]earch [D]iagnostics" },
+    ["<leader>/"] = { "<Cmd>Telescope current_buffer_fuzzy_find<CR>", "[S]earch In Buffer" },
+    ["<leader>sl"] = { "<Cmd>Telescope lsp_definitions<CR>", "[S]earch [L]sp" },
+    ["<leader>t"] = { "<Cmd>NvimTreeToggle<CR>", "[T]oggle [T]ree" },
+    ["q"] = { "<Nop>" },
+  },
+  i = {
+    ["kj"] = { "<Esc>", "Exit insert mode", opts = { nowait = true, noremap = true } },
+  },
+}
+
+-- more keybinds!
+
+return M
