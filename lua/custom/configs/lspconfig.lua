d343659f20e0050fad777598dc95da78bbc1427a
Roee Jukin <roeej@pm.me>
2023-09-05 17:22:15 +0300

    feat(lspconfig): Add support for tailwindcss in lspconfig.lua

--- lua/custom/configs/lspconfig.lua
+++ lua/custom/configs/lspconfig.lua
@@ -4,14 +4,19 @@ local capabilities = require("plugins.configs.lspconfig").capabilities
 local lspconfig = require "lspconfig"
 
 -- if you just want default config for the servers then put them in a table
-local servers = { "html", "cssls", "tsserver", "svelte", "clangd", "gopls", "rust_analyzer"}
+local servers = { "html", "cssls", "tsserver", "tailwindcss", "svelte", "clangd", "gopls", "rust_analyzer" }
 
 for _, lsp in ipairs(servers) do
   lspconfig[lsp].setup {
     on_attach = on_attach,
-    capabilities = capabilities,
+    capabilities = vim.tbl_deep_extend("force", capabilities, require("cmp_nvim_lsp").default_capabilities()),
   }
 end
 
--- 
+--
 -- lspconfig.pyright.setup { blabla}
+lspconfig["tsserver"].setup {
+  on_attach = on_attach,
+  capabilities = vim.tbl_deep_extend("force", capabilities, require("cmp_nvim_lsp").default_capabilities()),
+  filetypes = { "javascript", "javascriptreact", "javascript.jsx", "typescript", "typescriptreact", "typescript.tsx", "svelte" },
+}
